06231c7b19a15cd48bb07d7f7c9b3e0fcfd94a18
Cade Chaney <cadechaney@live.com>
2023-07-04 18:06:00 -0500

    Add yosemite painting

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,20 +1,30 @@
 import './AboutMe.css'
+import teachingVideo from '../../assets/teachingVideo'
+
+
 
 function AboutMe() {
 
-  return (
-    <>
-      <h1>About me</h1>
-      <section className='scroll-bar'>
-        <p className='option'>Contact Me</p>
-        <p className='option'>Resume</p>
-        <p className='option'>About this site</p>
-        <p className='option'>Portfolio</p>
-      </section>
-      <section className='info-container'>
+  const teachingVideo = require('../../assets/teachingVideo.gif')
 
+  return (
+    <div className='main'>
+      <header>
+        <h1>About me</h1>
+        <section className='scroll-bar'>
+          <p className='option'>Contact Me</p>
+          <p className='option'>Resume</p>
+          <p className='option'>About this site</p>
+          <p className='option'>Portfolio</p>
+        </section>
+      </header>
+      <section className='info-container'>
+        <h2>Early Life</h2>
+        <div className='info-card-1'>
+         
+        </div>
       </section>
-    </>
+    </div>
   )
 }
 
