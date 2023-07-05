bb5312b206a6811a3ce143f9cfe336def28aad95
Cade Chaney <cadechaney@live.com>
2023-07-04 20:39:14 -0500

    Add gif of me teaching and details

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,11 +1,11 @@
 import './AboutMe.css'
-import teachingVideo from '../../assets/teachingVideo'
+// import teachingVideo from '../../assets/teachingVideo'
 
 
+const teachingVideo = require('../../assets/teachingVideo.gif')
 
 function AboutMe() {
 
-  const teachingVideo = require('../../assets/teachingVideo.gif')
 
   return (
     <div className='main'>
@@ -21,7 +21,14 @@ function AboutMe() {
       <section className='info-container'>
         <h2>Early Life</h2>
         <div className='info-card-1'>
-         
+          <img className='teaching-video' src={teachingVideo}></img>
+          <article className='details-1'>
+            <h3>
+              I have found myself my entire life chasing efforts in being great at anything I do. My greatest personal example of this is my Brazilian Jiu Jitsu training.
+              With Jiu Jitsu, I fell in love with the process of learning complex techniques and being put under pressure in competition. What I love even more than that is my ability to then
+              introduce and teach these aspects, especially to the younger generations so they can grow confidence, be proud of their achievements, and learn respect for others.
+            </h3>
+          </article>
         </div>
       </section>
     </div>
