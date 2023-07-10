9ebdee7d3b3ce0b9f2e47d5f9abffdaa1d13ab9e
Cade Chaney <cadechaney@live.com>
2023-07-10 07:24:27 -0500

    Add contact information

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -2,9 +2,13 @@ import './ContactMe.css'
 
 function ContactMe() {
   return (
-    <>
-      <p>Contact Page</p>
-    </>
+    <div className='contact-info'>
+      <h2 className='info-detail'>Email: cadechaney@live.com</h2>
+      <h2 className='info-detail'>Phone: (720)490-8630</h2>
+      <h2 className='info-detail'>
+        <a href="https://www.linkedin.com/in/jonathan-chaney-a333ba235/" className='linkedin-link'>LinkedIn</a>
+      </h2>
+    </div>
   )
 }
 
