dae7034c180d69ba3aacbc4e046dca50a3e47812
Cade Chaney <cadechaney@live.com>
2023-07-21 08:30:13 -0500

    Add basic functional component layout for portfolio

---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,15 @@
+import './Portfolio.css'
+import React from 'react'
+import Header from '../Header/Header'
+
+
+const Portfolio: React.FC<PortfolioProps> = ({ portfolioTitle }) => {
+  
+  return (
+    <>
+      <p>portfolio</p>
+    </>
+  )
+}
+
+export default Portfolio
