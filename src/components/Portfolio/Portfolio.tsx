5fc699544b9718b5fc45bab0f3d27772c7a054f3
Cade Chaney <cadechaney@live.com>
2023-07-21 11:48:21 -0500

    Add layout to flight log section in portfolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -17,9 +17,29 @@ const Portfolio: React.FC<PortfolioProps> = ({ portfolioTitle }) => {
           {/* <img className='teaching-video' src={teachingVideo}></img> */}
           <article className='details-section-1'>
             <h1>Flight Log</h1>
-            <h3>
-              
-            </h3>
+            <h4>
+            {'\u25AA'}Full Stack Group Project | 75 Hours | GitHub Front End Repo | Deployed App
+            </h4>
+            <h5>
+            {'\u25AA'}Front End Technology - React Native
+            </h5>
+            <h5>
+              {'\u25AA'}Project Purpose:
+            </h5>
+            <h6>
+              - Problem: User is a pilot that needs an efficient and easy way to log flight information
+            </h6>
+            <h6>
+              - Answer: User now has a mobile application they can have on their phone to log flight info easily and reliably
+            </h6>
+            <h5>
+              {'\u25AA'}Description:
+            </h5>
+            <h6>
+              - With a Full Stack team having Front End building with React Native and Back End using Python, we created an app that pilots can use to log their flights 
+              on their mobile device
+            </h6>
+
           </article>
         </div>
         <div className='portfolio-card-2'>
