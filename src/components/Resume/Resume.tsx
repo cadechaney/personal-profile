c40cf844fbb1dbc9098f065adc5189e59240d457
Cade Chaney <cadechaney@live.com>
2023-07-21 07:33:52 -0500

    Add basic layout of resume functional component

---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,19 @@
+import './Resume.css'
+import React from 'react'
+
+
+interface ResumeProps {
+  resumeTitle: string;
+}
+
+const Resume: React.FC<ResumeProps> = ({ resumeTitle }) => {
+
+  return (
+    <>
+      <p>resume</p>
+    
+    </>
+  )
+}
+
+export default Resume
